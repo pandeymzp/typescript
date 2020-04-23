2de13a6b07a1565043e23a03450e93ce834b3d0a
pandeymzp <64181688+pandeymzp@users.noreply.github.com>
2020-04-23 09:04:32 +0530

    Add files via upload

---
+++ test.ts
@@ -0,0 +1,19 @@
+
+// Var tyoe define
+let valstring : string = "this is string"
+let valnumber : number = 1;
+let valboolean :boolean = true;
+let valarrystr : string[] = ["s", "D", "f"]
+let valarrynum : number[] = [1, 2, 3, 4]
+let valany : any = "Any type"
+// console.log(valarrynum)
+
+
+
+
+
+
+
+
+
+
